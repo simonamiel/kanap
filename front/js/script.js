715dca4b8ac0bbaab465bec385583fa7fa10b10e
simonamiel <sim.47photmail.fr>
2022-08-02 17:31:25 +0200

    Add product on home page whith js first part

--- front/js/script.js
+++ front/js/script.js
@@ -1,8 +1,26 @@
 let productData = [];
 
-async function productDatabase (){
-    let result = await fetch("http://localhost:3000/api/products");
-    let data = await result.json();
-    console.log(data);
-}
-productDatabase();
+/*Add array and data of API part 2*/
+const dataArray = async () => {
+    await fetch("http://localhost:3000/api/products")
+    .then(result => result.json())
+    .then(data => {
+        kanapData = data
+        console.log(kanapData);
+    });
+};
+
+/*Add products on home page part 3*/
+const addProduct = async () => {
+    await dataArray();
+    document.getElementById("items").innerHTML = kanapData.map((kanap) => `
+    <a href="./product.html?id=42" class="items a">
+        <article class="items article">
+              <img src="${kanap.imageUrl}" alt="${kanap.altTxt}" class="items article img">
+              <h3 class="productName items article h3">${kanap.name}</h3>
+              <p class="productDescription items article p">${kanap.description}</p>
+        </article>
+    </a>
+    `).join("");
+};
+addProduct ()
