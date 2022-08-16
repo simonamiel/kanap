ee525cc1d1ca9276b5664f6b3ee46b4fe23824a4
simonamiel <sim.47photmail.fr>
2022-08-16 17:03:29 +0200

    Manage product modification and deletion in the cart page step 9 second part

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,7 @@
 /*Retrieve product informations from localstorage and product page*/
 let addkanap = JSON.parse(localStorage.getItem("product"));
 
-/*insertion of items in the shopping cart and display of information*/
+/*insertion of items in the shopping cart and display of information part 7*/
 const addcart = async () => {
 	if (addkanap) {
 		await addkanap;
@@ -32,11 +32,15 @@ const addcart = async () => {
               </article>
         `
 		);
+
+		moreQuantity();
+        supprQuantity();
+       
 	}
 };
 addcart();
 
-/*Add the total of pieces in function of number of piece in cart*/
+/*Add the total of pieces in function of number of piece in cart part 8*/
 console.log(addkanap); //addKanap = Localstorage (see above)
 
 let kanapQuantityTotal = [];
@@ -52,4 +56,120 @@ if (addkanap) {
 	).textContent = `${eval(kanapQuantityTotal.join("+"))}`);
 	console.log(totalkanap);
 }
-/*Add the total price in function of number of pieces and unit price*/
+/*Add the total price in function of number of pieces and unit price part 8*/
+let kanapPriceTotal = [];
+
+if (addkanap) {
+	addkanap.forEach((kanap) => {
+		kanapPriceTotal.push(kanap.price * kanap.quantitySelection);
+		console.log(kanapPriceTotal);
+	});
+	document.getElementById("totalPrice").innerHTML = kanapPriceTotal;
+	let totalkanapPrice = (document.getElementById(
+		"totalPrice"
+	).textContent = `${eval(kanapPriceTotal.join("+"))}`);
+	console.log(totalkanapPrice);
+}
+
+/*Modification of the more quantities in the basket part 9*/
+const moreQuantity = async (addcart) => {
+	await addcart;
+	console.log("modifQty");
+	let modificationMore = document.querySelectorAll(".cart__item");
+	console.log(modificationMore);
+	modificationMore.forEach((changeQtyMore) => {
+		changeQtyMore.addEventListener("change", () => {
+			console.log(changeQtyMore);
+            console.log(addkanap);
+            
+
+			/*comparison of localstorage with the basket whith an eventlistener on change*/
+			for (i = 0; i < addkanap.length; i++) {
+				if (
+					addkanap[i].idProductSelection == changeQtyMore.dataset.id &&
+					addkanap[i].colorSelection == changeQtyMore.dataset.color
+				)
+                {
+					return (
+                        /*For more quantity in input selection add more product to localstorage*/
+						addkanap[i].quantitySelection++,
+						console.log(addkanap[i]),
+						localStorage.setItem("product", JSON.stringify(addkanap)),
+						(document.querySelectorAll(".itemQuantity")[i].textContent =
+						addkanap[i].quantitySelection),
+                        (addkanap = JSON.parse(localStorage.getItem("product"))),
+                        console.log(addkanap),
+                        /*Update total of quantity list*/
+                        kanapQuantityTotal = [],
+                        addkanap.forEach((kanap) => {
+                        kanapQuantityTotal.push(kanap.quantitySelection),
+                        (document.getElementById("totalQuantity").innerHTML = kanapQuantityTotal),
+                        totalkanap = (document.getElementById("totalQuantity").textContent = `${eval(kanapQuantityTotal.join("+"))}`),
+	                    console.log(totalkanap)
+                        }),
+                        /*Update total of price list*/
+                        kanapPriceTotal = [],
+	                    addkanap.forEach((kanap) => {
+		                kanapPriceTotal.push(kanap.price * kanap.quantitySelection),
+		                document.getElementById("totalPrice").innerHTML = kanapPriceTotal,
+                        totalkanapPrice = (document.getElementById("totalPrice").textContent = `${eval(kanapPriceTotal.join("+"))}`),
+                        console.log(totalkanapPrice)
+	                    })
+                        );
+				}
+                
+            
+            }
+		})
+    })
+}
+/*Modification of the less quantities in the basket part 9*/
+/*const lessQuantity = async (addcart) => {
+    await addcart;
+	console.log("modifQty");
+    let modificationLess = document.querySelectorAll(".cart__item");
+	console.log(modificationLess);
+	modificationLess.forEach((changeQtyLess) => {
+		changeQtyLess.addEventListener("change", () => {
+			console.log(changeQtyLess);
+            console.log(addkanap);
+
+            let totalProductList = addkanap.length;
+
+            for(i=0; i < totalProductList; i++) {
+                console.log(totalProductList);
+                if (addkanap[i].quantitySelection == 1 && totalProductList != 1) {
+                    return (
+                        localStorage.removeItem("product"),
+                        (location.href = "cart.html"),
+                        console.log("remove")
+                    )
+                }
+
+            }
+        })
+    })
+}*/
+const supprQuantity = async (addcart) => {
+    await addcart;
+	console.log("supprQty");
+    let modificationSuppr = document.querySelectorAll(".cart__items >");
+	console.log(modificationSuppr);
+	modificationSuppr.forEach((changeQtySuppr) => {
+		changeQtySuppr.addEventListener("click", () => {
+            for (i = 0; i < addkanap.length; i++) {
+				if (
+					addkanap[i].idProductSelection == changeQtySuppr.dataset.id &&
+					addkanap[i].colorSelection == changeQtySuppr.dataset.color
+				) {
+                    return (
+                        addkanap.splice(i, 1),
+                        localStorage.setItem("product", JSON.stringify(addkanap)),
+                        location.href = "cart.html",
+                        console.log("test")
+                    )
+
+                }
+        }})
+    })
+}
