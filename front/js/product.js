acfd7ce5fcce7941b4614c04f68e8fff112fdb81
simonamiel <sim.47photmail.fr>
2022-08-19 15:00:35 +0200

    Manage product modification and deletion in the cart page step 9 fourth  part

--- front/js/product.js
+++ front/js/product.js
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams(queryId);
 const getId = queryId.toString().split("?").join("");
 console.log(getId);
 
-let dataproduct = [];
+let dataProduct = [];
 
 /*product insertion according to id */
 const productData = async () => {
@@ -13,69 +13,46 @@ const productData = async () => {
 		.then((data) => {
 			productInfo = data;
 			console.log(productInfo);
-		});
+            })    
 };
 
+
 /*insertion of product information according to the id */
-const addProduct = async () => {
-	await productData();
-	document.querySelector(".item__img").innerHTML = `
+async function addProduct() {
+    await productData();
+    document.querySelector(".item__img").innerHTML = `
     <img src="${productInfo.imageUrl}" alt="${productInfo.altTxt}"/>
     `;
-	document.getElementById("title").innerHTML = `
+    document.getElementById("title").innerHTML = `
     ${productInfo.name}
     `;
-	document.getElementById("price").innerHTML = `
+    document.getElementById("price").innerHTML = `
     ${productInfo.price}
     `;
-	document.getElementById("description").innerHTML = `
+    document.getElementById("description").innerHTML = `
     ${productInfo.description}
     `;
 
-	/*Selection of color in color's field*/
+    /*Selection of color in color's field*/
+    /*Selection of HTML's id="colors"*/
+    let selectColors = document.getElementById("colors");
 
-	/*Selection of HTML's id="colors"*/
-	let selectColors = document.getElementById("colors");
+    /*Read the array for each color and create an option element in HTML*/
+    productInfo.colors.forEach((colors) => {
+        //console.log(colors);
+        let addColors = document.createElement("option");
 
-	/*Read the array for each color and create an option element in HTML*/
-	productInfo.colors.forEach((colors) => {
-		//console.log(colors);
-		let addColors = document.createElement("option");
+        /*Insertion of "value" and "HTML text" data in each option tag for each color*/
+        addColors.value = `${colors}`;
+        addColors.innerHTML = `${colors}`;
 
-		/*Insertion of "value" and "HTML text" data in each option tag for each color*/
-		addColors.value = `${colors}`;
-		addColors.innerHTML = `${colors}`;
-
-		/*Indication that "addColors" is a child of "selectColors"*/
-		selectColors.appendChild(addColors);
-		//console.log(addColors);
-	});
-	addToCart(productInfo);
-};
+        /*Indication that "addColors" is a child of "selectColors"*/
+        selectColors.appendChild(addColors);
+        //console.log(addColors);
+    });
+    addToCart(productInfo);
+}
 addProduct();
-
-/*verification that the color and the quantity is well informed before putting it in the basket*/
-/*const checkProduct = async () => {
-	document
-		.getElementById("addToCart")
-		.addEventListener("click", function (eventColors) {
-			var select = document.getElementById("colors");
-			var choice = select.selectedIndex;
-			var value = select.options[choice].value;
-			if (value === "") {
-				alert("Veuillez choisir une couleur");
-			}
-		});
-	document
-		.getElementById("addToCart")
-		.addEventListener("click", function (eventQuantity) {
-			var productQuantity = document.getElementById("quantity").value;
-			if (productQuantity <= 0 || productQuantity > 100) {
-				alert("Veuillez renseigner une quantité valide");
-			}
-		});
-};
-checkProduct();*/
 
 /*Transmission of items to the basket whith button "ajouter au panier"*/
 const addToCart = () => {
